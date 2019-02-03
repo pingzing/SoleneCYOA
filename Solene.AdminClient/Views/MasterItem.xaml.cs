2dce57c0709fef932e170d887d1da2b607e67d39
Neil McAlister <neil.mcalister@futurice.com>
2019-02-03 21:41:48 +0200

    Add timestamps to master list

--- Solene.AdminClient/Views/MasterItem.xaml.cs
+++ Solene.AdminClient/Views/MasterItem.xaml.cs
@@ -4,12 +4,18 @@ using Windows.UI.Xaml.Controls;
 using Windows.UI.Xaml;
 using System.Linq;
 using System.Diagnostics;
+using System.ComponentModel;
+using System.Runtime.CompilerServices;
 
 namespace Solene.AdminClient.Views
 {
-    public sealed partial class MasterItem : UserControl
-    {
+    public sealed partial class MasterItem : UserControl, INotifyPropertyChanged
+    {           
         public event EventHandler<AdminPlayerProfile> DeleteClick;        
+        public event PropertyChangedEventHandler PropertyChanged;
+
+        public AdminPlayerProfile BackingProfile => DataContext as AdminPlayerProfile;
+        public DateTimeOffset LastUpdated => BackingProfile?.Questions?.LastOrDefault()?.UpdatedTimestamp ?? new DateTimeOffset();
 
         public MasterItem()
         {
@@ -35,11 +41,18 @@ namespace Solene.AdminClient.Views
             {
                 VisualStateManager.GoToState(this, "Answered", false);
             }
+            RaisePropertyChanged(nameof(BackingProfile));
+            RaisePropertyChanged(nameof(LastUpdated));
         }
 
         private void DeleteItem_Click(object sender, RoutedEventArgs e)
         {
             DeleteClick?.Invoke(sender, this.DataContext as AdminPlayerProfile);
+        }
+
+        private void RaisePropertyChanged([CallerMemberName]string propertyName = null)
+        {
+            PropertyChanged?.Invoke(this, new PropertyChangedEventArgs(propertyName));
         }
     }
 }
