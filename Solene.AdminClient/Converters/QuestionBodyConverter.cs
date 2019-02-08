40c80545fd962c3ea2f143f05817eb067ed06092
Neil McAlister <neil.mcalister@futurice.com>
2019-02-08 17:55:05 +0200

    Add some code for formatting to the Admin client

---
+++ Solene.AdminClient/Converters/QuestionBodyConverter.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Windows.UI.Xaml.Data;
+
+namespace Solene.AdminClient.Converters
+{
+    public class QuestionBodyConverter : IValueConverter
+    {
+        public object Convert(object value, Type targetType, object parameter, string language)
+        {
+            throw new NotImplementedException();
+        }
+
+        public object ConvertBack(object value, Type targetType, object parameter, string language)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
---
+++ Solene.AdminClient/Services/ParsingService.cs
@@ -0,0 +1,64 @@
+﻿using Solene.SimpleParser;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text.RegularExpressions;
+using Windows.UI.Text;
+using Windows.UI.Xaml.Documents;
+
+namespace Solene.AdminClient.Services
+{
+    public class ParsingService
+    {
+        public IEnumerable<Run> FormatBody(string text)
+        {
+            var runs = new List<Run>();
+            var spans = Parser.ApplyRulesToText(text);
+            if (!spans.Any())
+            {
+                return new[] { new Run() { Text = text } };
+            }
+
+            int currentIndex = 0;
+            foreach (var span in spans)
+            {
+                if (currentIndex < span.StartIndex)
+                {
+                    runs.Add(new Run
+                    {
+                        Text = text.Substring(currentIndex, span.StartIndex - currentIndex),
+                    });
+                }
+
+                runs.Add(new Run
+                {
+                    Text = StripUnprintableCharacters(span.Value),
+                    FontWeight = span.RuleType == RuleType.Bold
+                        ? FontWeights.Bold
+                        : FontWeights.Normal,
+                    FontStyle = span.RuleType == RuleType.Italic
+                        ? FontStyle.Italic
+                        : FontStyle.Normal,
+                });
+
+                int indexDiff = text.Length - currentIndex;
+                if (indexDiff > 0)
+                {
+                    runs.Add(new Run
+                    {
+                        Text = text.Substring(currentIndex, indexDiff)
+                    });
+                }
+            }
+
+            return runs;
+        }
+
+        private static Regex _unprintables = new Regex(@"[\*_]", RegexOptions.Compiled);
+        private static string StripUnprintableCharacters(string text)
+        {
+            return _unprintables.Replace(text, "");
+        }
+    }
+}
+
+
